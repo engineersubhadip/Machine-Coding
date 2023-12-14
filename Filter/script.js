deaf27b61222ad38d39602820a29ca60e8329c39
Subhadip Das <subhadipdas911@gmail.com>
2023-12-14 13:20:51 +0530

    Implemented the Filter Functionality Fully

--- Filter/script.js
+++ Filter/script.js
@@ -1,1 +1,26 @@
-console.log("Heloo");
+let selectInputBox = document.querySelector("select");
+
+
+selectInputBox.addEventListener("change",filtration);
+
+
+function filtration(event){
+      let currentValue = event.target.value;
+      let currentMovieList = document.querySelectorAll(".movies p");
+
+      // console.log(currentMovieList[0].getAttribute("data-category"));
+
+      for (let i=0; i<currentMovieList.length; i++){
+            let movieCategory = currentMovieList[i].getAttribute("data-category");
+            if (currentValue == "none" || movieCategory == currentValue){
+                  // Show the Movie
+                  currentMovieList[i].parentElement.style.display = "block";
+            }else{
+                  // Hide the Movie
+                  currentMovieList[i].parentElement.style.display = "none";
+            }
+      }
+
+      
+      
+}
