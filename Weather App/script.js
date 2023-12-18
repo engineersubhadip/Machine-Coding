fcc419c77c59ef6eebaa59a6ad3fe91e7d4bf880
Subhadip Das <subhadipdas911@gmail.com>
2023-12-18 13:13:36 +0530

    Implemented the check that if the locatioon search bar is empty the Search Button will be disabled

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,1 +1,14 @@
-console.log("JS Invoked");
+let locationInputBar = document.querySelector("#location-input-bar");
+
+let searchButton = document.querySelector(".location-search");
+
+locationInputBar.addEventListener("keyup", function(e){
+      let locationValue = locationInputBar.value;
+      
+      if (locationValue.length == 0){
+            searchButton.classList.add("disabled");
+      }else{
+            searchButton.classList.remove("disabled");
+      }
+});
+
