13b0b7b351f2ce0e7288e3ccbe9e897b8ee4aee4
Subhadip Das <subhadipdas911@gmail.com>
2023-12-21 14:10:15 +0530

    We have implemented the functionality to create New ticket

--- Kanban Board/script.js
+++ Kanban Board/script.js
@@ -1,6 +1,7 @@
 let addBtn = document.querySelector(".add-btn");
 let modalContainer = document.querySelector(".modal-container");
 let isModalHidden = true;
+let parentTicketHolder = document.querySelector(".row");
 
 // On Click of the Add Btn We will show/hide the Modal
 
@@ -29,3 +30,41 @@ delBtn.addEventListener("click", function(e){
         delBtnColor = "black";
     }
 })
+
+// If the user enters anything on the task input and presses Enter a Ticket should Generate
+
+let taskInput = document.querySelector(".task-input");
+
+taskInput.addEventListener("keyup",function(e){
+    if (e.key == "Enter"){
+        let currentValue = taskInput.value;
+        createTicket(currentValue);
+        modalContainer.style.display = "none";
+        isModalHidden = true;
+        taskInput.value = "";
+    }
+})
+
+
+function createTicket(currentValue){
+    let ticket = document.createElement("div");
+    let bannerColor = document.createElement("div");
+    let ticketNumber = document.createElement("div");
+    let ticketDescription = document.createElement("div");
+    let lockUnlock = document.createElement("div");
+
+    ticket.classList.add("ticket");
+    bannerColor.classList.add("header-banner-color", "text-center");
+    ticketNumber.classList.add("ticket-number");
+    ticketDescription.classList.add("ticket-description");
+    lockUnlock.classList.add("ock-unlock");
+
+    ticket.appendChild(bannerColor);
+    ticket.appendChild(ticketNumber);
+    ticket.appendChild(ticketDescription);
+    ticket.appendChild(lockUnlock);
+
+    ticketDescription.textContent = currentValue;
+
+    parentTicketHolder.appendChild(ticket);
+}
