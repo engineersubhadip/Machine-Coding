5ec07a662ae3c177b1217c351ee0329377c8c9f8
Subhadip Das <subhadipdas911@gmail.com>
2023-12-21 13:34:09 +0530

    Added the Functionality to toggle between the Delete Button Colors

--- Kanban Board/script.js
+++ Kanban Board/script.js
@@ -1,8 +1,8 @@
-console.log("Linked the JS");
-
 let addBtn = document.querySelector(".add-btn");
 let modalContainer = document.querySelector(".modal-container");
 let isModalHidden = true;
+
+// On Click of the Add Btn We will show/hide the Modal
 
 addBtn.addEventListener("click", function(e){
     if (isModalHidden){
@@ -13,3 +13,19 @@ addBtn.addEventListener("click", function(e){
         isModalHidden = true;  
     }
 });
+
+// On Click of the Del Button the button color will change
+// If someone clicks for the first time it will change to "red" and if someone clicks on it again it will change to "black";
+
+let delBtn = document.querySelector(".del-btn");
+let delBtnColor = "black";
+
+delBtn.addEventListener("click", function(e){
+    if (delBtnColor == "black"){
+        delBtn.style.color = "red";
+        delBtnColor = "red";
+    }else if(delBtnColor == "red"){
+        delBtn.style.color = "black";
+        delBtnColor = "black";
+    }
+})
