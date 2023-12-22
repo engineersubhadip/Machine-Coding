875fe2fe1b968715da2d8fed0dc512b196ee2887
Subhadip Das <subhadipdas911@gmail.com>
2023-12-22 13:28:05 +0530

    Fixed the Bug for the Draw Sceanrio

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded",function(){
     let outer = document.querySelector("#outer");
     let chance = false; // If Chance is False we put "O", if true we put "X"
-    let arr = Array(9).fill(9);
+    let arr = Array(9).fill(undefined);
 
-    
+    let flag = false; // This flag will be used for getting who is the winner
 
     outer.addEventListener("click",function(e){
         let cell = e.target;
@@ -22,7 +22,16 @@ document.addEventListener("DOMContentLoaded",function(){
             arr[cellNumber]="O";
             winningCombo("O")
         }
-        document.getElementById("result").textContent;
+
+        if (flag){ // We have a winner
+            let finalResult = document.getElementById("result");
+            finalResult.textContent;
+            
+            // Clearing out the UI :- The UI will be cleared after 1s
+            setTimeout(resetBoard,1000);
+        
+        }
+
     });
 
     
@@ -31,34 +40,54 @@ document.addEventListener("DOMContentLoaded",function(){
         if(arr[0] == char && arr[1] == char && arr[2] == char) {
             // the f0th row is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[3] == char && arr[4] == char && arr[5] == char) {
             // the 1st row is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[6] == char && arr[7] == char && arr[8] == char) {
             // the 2nd row is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[0] == char && arr[3] == char && arr[6] == char) {
             // the 0th col is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[1] == char && arr[4] == char && arr[7] == char) {
             // the 1st col is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
-        else if(arr[2] == char && arr[5] == char && arr[0] == char) {
+        else if(arr[2] == char && arr[5] == char && arr[8] == char) {
             // the 2nd col is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[0] == char && arr[4] == char && arr[8] == char) {
             // the 1st col is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
         }
         else if(arr[2] == char && arr[4] == char && arr[6] == char) {
             // the 2nd col is having the char;
             result.textContent = `${char} wins`;
+            flag = true;
+        }
+    }
+
+
+    function resetBoard() {
+        let result = document.getElementById("result");
+        result.textContent = ""; // Clear the result message
+        flag = false; // Reset the flag
+        arr.fill(undefined); // Clear the array
+        let innerCellArr = document.querySelectorAll(".inner");
+        for (let i = 0; i < innerCellArr.length; i++) {
+            innerCellArr[i].textContent = ""; // Clear the board UI
         }
     }
 
