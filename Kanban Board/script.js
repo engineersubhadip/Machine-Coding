5c08634ee7466d69e4ca50e4d8b6376a31142cd2
Subhadip Das <subhadipdas911@gmail.com>
2023-12-23 12:34:09 +0530

    Added the lock/unlock button during ticket creation

--- Kanban Board/script.js
+++ Kanban Board/script.js
@@ -19,35 +19,52 @@ addBtn.addEventListener("click", function(e){
 
 let taskInput = document.querySelector(".task-input");
 
+let priorityColor = document.querySelectorAll(".priority-color");
+
+
 taskInput.addEventListener("keyup",function(e){
+
+    // Now we will select the color from color Priority:-
+    // let colorPriority = document.querySelector(".color-priority");
+
+    // colorPriority.addEventListener("click",function(event){
     if (e.key == "Enter"){
         let currentValue = taskInput.value;
         createTicket(currentValue);
         modalContainer.style.display = "none";
         isModalHidden = true;
         taskInput.value = "";
-    }
+        }
+    // })
 })
 
 
 function createTicket(currentValue){
+    var uid = new ShortUniqueId();
+    let id = uid.rnd();
     let ticket = document.createElement("div");
     let bannerColor = document.createElement("div");
     let ticketNumber = document.createElement("div");
     let ticketDescription = document.createElement("div");
     let lockUnlock = document.createElement("div");
+    let lockUnlockImage = document.createElement("i");
+
 
     ticket.classList.add("ticket");
     bannerColor.classList.add("header-banner-color", "text-center");
     ticketNumber.classList.add("ticket-number");
     ticketDescription.classList.add("ticket-description");
     lockUnlock.classList.add("ock-unlock");
+    lockUnlockImage.classList.add("fa-solid","fa-lock");
 
+    lockUnlock.appendChild(lockUnlockImage);
     ticket.appendChild(bannerColor);
     ticket.appendChild(ticketNumber);
     ticket.appendChild(ticketDescription);
     ticket.appendChild(lockUnlock);
 
+    ticketNumber.textContent = `#${id}`;
+    bannerColor.style.backgroundColor = "greenyellow";
     ticketDescription.textContent = currentValue;
 
     ticket.addEventListener("click",delTicket); // For every ticket we are generating has a click event listener attached to it.
