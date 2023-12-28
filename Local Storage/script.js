1dab1d7711a9d0cad26bd3b31198e38113b3c262
Subhadip Das <subhadipdas911@gmail.com>
2023-12-28 14:04:49 +0530

    Updated the script file with some relevant comments

--- Local Storage/script.js
+++ Local Storage/script.js
@@ -1,5 +1,9 @@
 let array = [12,13,14,15];
 
+// How to store inside the Local Storage
+
 let arrayStr = JSON.stringify(array);
 
 localStorage.setItem("Array",arrayStr);
+
+// How to access data from Local Storage;
