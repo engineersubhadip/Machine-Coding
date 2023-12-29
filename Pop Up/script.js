acb27b719de0ddf910b41be6c43c75a2de9e9de1
Subhadip Das <subhadipdas911@gmail.com>
2023-12-29 21:37:09 +0530

    Pop Up Application

---
+++ Pop Up/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    let submitBtn = document.querySelector(".submit-btn button");
+
+
+    submitBtn.addEventListener("click",function(){
+    
+    submitBtn.style.z
+    
+    let rowTwo = document.querySelector(".row-two");
+    rowTwo.style.display = "block";
+
+    let okBtn = document.querySelector(".ok-btn button");
+
+    okBtn.addEventListener("click",function(){
+        rowTwo.style.display = "none";
+    });
+    });
+
+})
+
