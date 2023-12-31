f190c7ca5027ddd775f78d667c7b5cec2de9058a
Subhadip Das <subhadipdas911@gmail.com>
2023-12-31 12:07:41 +0530

    Implemented the addition and subtraction

---
+++ Counter/script.js
@@ -0,0 +1,37 @@
+let finalDisplay = document.querySelector("#number");
+
+let incDecField = document.querySelector("#increment");
+
+let finalOutput = 0;
+
+let addBtn = document.querySelector("#add");
+let subBtn = document.querySelector("#subtract");
+
+addBtn.addEventListener("click",function(e){
+    
+    // When this button will be clicked. We will take out the value from the incDecField and add it to the finalOutput
+
+    let userValue = parseInt(incDecField.value);
+    
+    if (userValue >= 1 && userValue <= 10){
+        finalOutput = finalOutput + userValue;
+        finalDisplay.textContent = finalOutput;
+    };
+});
+
+subBtn.addEventListener("click",function(e){
+    // When this button will be clicked. We will take out the value from the incDecField and subtract it to the finalOutput.
+
+    let userValue = parseInt(incDecField.value);
+
+    if (userValue >= 1 && userValue <= 10){
+        finalOutput = finalOutput - userValue;
+
+        if (finalOutput < 0){
+            finalDisplay.textContent = 0;
+            finalOutput = 0;
+        }else{
+            finalDisplay.textContent = finalOutput;
+        }
+    };
+});
