a0f8aef952af6ee195b7185b9158c1c60385f755
Subhadip Das <subhadipdas911@gmail.com>
2024-01-02 12:54:01 +0530

    Removed an extra loop by applying an atrribute inside HTMl

--- Star Component/script.js
+++ Star Component/script.js
@@ -9,23 +9,16 @@ let ratingCounter = document.querySelector(".rating");
 starParent.addEventListener("click",function(e){
     if (e.target.classList.contains("star")){
         let selectedStar = e.target;
-        let selectedIndex = 0;
+        let selectedIndex = e.target.getAttribute("position");
 
         for (let k=0; k<starList.length; k++){
             starList[k].classList.remove("active");
         }
         
-        for (let i=0; i<starList.length; i++){
-            if (starList[i] == selectedStar){
-                selectedIndex = i;
-                break;
-            };
-        };
-
-        for (let j=0; j<=selectedIndex; j++){
+        for (let j=0; j<selectedIndex; j++){
             starList[j].classList.add("active");
         }
 
-        ratingCounter.innerText = `Rating : ${selectedIndex+1}`;
+        ratingCounter.innerText = `Rating : ${selectedIndex}`;
     }
 })
