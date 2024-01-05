a57a9d333b1b0096a689b8123f729accc77d3f85
Subhadip Das <subhadipdas911@gmail.com>
2024-01-05 21:09:48 +0530

    Added the data

---
+++ Chat Application/data.js
@@ -0,0 +1,218 @@
+const message = [
+    {
+      "id": 1,
+      "title": "first title",
+      "imageURL": "https://i.pravatar.cc/300",
+      "orderId": "OD123",
+      "messageList": [
+        {
+          "messageId": "msg1",
+          "message": "Hi",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg2",
+          "message": "need assistance",
+          "messageType": "text"
+        },
+      ]
+    },
+    {
+      "id": 2,
+      "title": "second title",
+      "imageURL": "https://i.pravatar.cc/200",
+      "orderId": "OD456",
+      "messageList": [
+        {
+          "messageId": "msg3",
+          "message": "Hello",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg4",
+          "message": "urgent request",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "title": "third title",
+      "imageURL": "https://i.pravatar.cc/340",
+      "orderId": "OD789",
+      "messageList": [
+        {
+          "messageId": "msg5",
+          "message": "Hey there",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg6",
+          "message": "can you help?",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "title": "fourth title",
+      "imageURL": "https://i.pravatar.cc/300",
+      "orderId": "OD1011",
+      "messageList": [
+        {
+          "messageId": "msg7",
+          "message": "Greetings",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg8",
+          "message": "looking for assistance",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 5,
+      "title": "fifth title",
+      "imageURL": "https://i.pravatar.cc/350",
+      "orderId": "OD1213",
+      "messageList": [
+        {
+          "messageId": "msg9",
+          "message": "Hi there",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg10",
+          "message": "need help",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 6,
+      "title": "sixth title",
+      "imageURL": "https://i.pravatar.cc/360",
+      "orderId": "OD1415",
+      "messageList": [
+        {
+          "messageId": "msg11",
+          "message": "Greetings",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg12",
+          "message": "urgent matter",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "title": "seventh title",
+      "imageURL": "https://i.pravatar.cc/370",
+      "orderId": "OD1617",
+      "messageList": [
+        {
+          "messageId": "msg13",
+          "message": "Hello there",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg14",
+          "message": "require assistance",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 8,
+      "title": "eighth title",
+      "imageURL": "https://i.pravatar.cc/380",
+      "orderId": "OD1819",
+      "messageList": [
+        {
+          "messageId": "msg15",
+          "message": "Hi!",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg16",
+          "message": "need your help",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 9,
+      "title": "ninth title",
+      "imageURL": "https://i.pravatar.cc/390",
+      "orderId": "OD2021",
+      "messageList": [
+        {
+          "messageId": "msg17",
+          "message": "Hey!",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg18",
+          "message": "require assistance urgently",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 10,
+      "title": "tenth title",
+      "imageURL": "https://i.pravatar.cc/280",
+      "orderId": "OD2223",
+      "messageList": [
+        {
+          "messageId": "msg19",
+          "message": "Hello!",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg20",
+          "message": "need immediate help",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 11,
+      "title": "Eleventh title",
+      "imageURL": "https://i.pravatar.cc/220",
+      "orderId": "OD2021",
+      "messageList": [
+        {
+          "messageId": "msg17",
+          "message": "Hey!",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg18",
+          "message": "require assistance urgently",
+          "messageType": "text"
+        }
+      ]
+    },
+    {
+      "id": 12,
+      "title": "Twelve title",
+      "imageURL": "https://i.pravatar.cc/230",
+      "orderId": "OD2223",
+      "messageList": [
+        {
+          "messageId": "msg19",
+          "message": "Hello!",
+          "messageType": "text"
+        },
+        {
+          "messageId": "msg20",
+          "message": "need immediate help",
+          "messageType": "text"
+        }
+      ]
+    }
+  ];
