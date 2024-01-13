d9a3fc9a880bea240c823028029f340e731d462f
Subhadip Das <subhadipdas911@gmail.com>
2024-01-13 23:30:29 +0530

    Initial Version of the Progress Bar

---
+++ Progress Bar/script.js
@@ -0,0 +1,44 @@
+let start = document.querySelector(".start");
+let stop = document.querySelector(".stop");
+let reset = document.querySelector(".reset");
+
+let progressBar = document.querySelector(".blue-loader");
+
+let timerID = undefined;
+
+function fillBar(){
+    timerID = setInterval(function(){
+        let width = captureWidth();
+        width = width+5;
+        progressBar.style.width=`${width}%`;
+
+        if (width == 100){
+            stopFilling(timerID);
+        }
+    },1000);
+};
+
+function captureWidth(){
+    let currentWidth = Number(progressBar.style.width.split("%")[0]);
+    return currentWidth;
+}
+
+function stopFilling(timerID){
+    clearInterval(timerID);
+    start.removeAttribute("disabled");
+}
+
+
+start.addEventListener("click",function(e){
+    fillBar();
+    start.setAttribute("disabled","true");
+})
+
+stop.addEventListener("click",function(){
+    stopFilling(timerID);
+});
+
+reset.addEventListener("click",function(){
+    stopFilling(timerID);
+    progressBar.style.width = `0%`;
+})
