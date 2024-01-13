2101587cb8efbdb6132bc7692ea4738e29781863
Subhadip Das <subhadipdas911@gmail.com>
2024-01-13 19:01:06 +0530

    Implemented the functionality that unless the all the fields are filled the SUBMIT Button will be disabled in the FORM

---
+++ Submit Disable/script.js
@@ -0,0 +1,28 @@
+let inputField = document.querySelectorAll("input");
+
+let submitBtn = document.querySelector("button");
+
+let isSubmit = false;
+
+for (let i=0; i<inputField.length; i++){
+    inputField[i].addEventListener("keyup",function(e){
+        let flagRes = checkFields();
+        if (flagRes){
+            submitBtn.removeAttribute("disabled");
+        }else{
+            submitBtn.setAttribute("disabled","true");
+        }
+    });
+};
+
+function checkFields(){
+    for (let i=0; i<inputField.length; i++){
+        let currentValue = inputField[i].value;
+        if (currentValue.length == 0){
+            isSubmit = false;
+            return isSubmit;
+        }
+    }
+    isSubmit = true;
+    return isSubmit;
+}
