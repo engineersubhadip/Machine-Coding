9ba77529936281e6a7debf37eb5ec5d1a3802e58
Subhadip Das <subhadipdas911@gmail.com>
2024-01-14 12:42:03 +0530

    Implemented the Start Timer Function

---
+++ CountDown App/script.js
@@ -0,0 +1,33 @@
+let min = document.querySelector(".min");
+let sec = document.querySelector(".sec");
+let milisec = document.querySelector(".milisec");
+
+let start = document.querySelector(".start");
+let stop = document.querySelector(".stop");
+let reset = document.querySelector(".reset");
+
+let timerId = undefined;
+
+function startTimer(){
+    let currValMil = Number(milisec.textContent);
+    let currValSec = Number(sec.textContent);
+    let currValMin = Number(min.textContent);
+
+    timerId = setInterval(function(){
+        currValMil+=1;
+        if (currValMil == 1000){
+            currValMil=0;
+            currValSec+=1;
+            if (currValSec == 60){
+                currValSec = 0;
+                currValMin+=1;
+                min.textContent = currValMin;
+            }
+            sec.textContent = currValSec;
+        }
+        milisec.textContent=currValMil;
+    },1);
+
+};
+
+// startTimer();
