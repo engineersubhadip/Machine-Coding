669cbd4df9fb32f02e688cca6883d949efb44beb
Subhadip Das <subhadipdas911@gmail.com>
2024-02-17 23:07:08 +0530

    Initial HTML,CSS applied

--- practice/script.js
+++ practice/script.js
@@ -1,8 +1,1 @@
-
-let row = document.querySelector(".row");
-
-row.addEventListener("click",function(e){
-      if (e.target.classList.contains("inner")){
-            console.log(e.target);
-      }
-})
+console.log("Linked");
