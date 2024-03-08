161efbccd8a47e3a4b5dfb7b84c0ae45b7bb9f45
Subhadip Das <subhadipdas911@gmail.com>
2024-03-09 01:48:23 +0530

    Complete Implementation of Show Hide Password

---
+++ Show-Hide-Password/script.js
@@ -0,0 +1,23 @@
+// console.log("Linked");
+
+let inputField = document.querySelector(".input-field");
+
+let isActive = false;
+
+let crossEyeIcon = document.querySelector(".fa-eye-slash");
+
+let eyeIcon = document.querySelector(".fa-eye");
+
+crossEyeIcon.addEventListener("click",function(e){
+    if(!isActive){
+        isActive = true;
+        crossEyeIcon.classList.remove("fa-eye-slash");
+        crossEyeIcon.classList.add("fa-eye");
+        inputField.setAttribute("type","text");
+    }else{
+        isActive = false;
+        crossEyeIcon.classList.remove("fa-eye");
+        crossEyeIcon.classList.add("fa-eye-slash");
+        inputField.setAttribute("type","password");
+    }
+})
