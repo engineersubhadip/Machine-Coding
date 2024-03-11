6f9cd18d43d773f054c7831b11017c2d3b3f0d2b
Subhadip Das <subhadipdas911@gmail.com>
2024-03-11 11:24:25 +0530

    Implemented the Animation Effect via setInterval

--- Animation Effect/script.js
+++ Animation Effect/script.js
@@ -1,1 +1,15 @@
 console.log("Linked");
+
+let targetElement = document.querySelector("#container");
+let count = 0;
+
+
+setInterval(function(){
+    if (count%2 == 0){
+        targetElement.style.backgroundColor = "blue";
+        count+=1;
+    }else{
+        targetElement.style.backgroundColor = "red";
+        count+=1;
+    }
+},500);
